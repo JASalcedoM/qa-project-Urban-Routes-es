8bf50d2df0da6388382387eef23f4468f2a0231d
JASalcedoM <a.salcedo.morales@outlook.com>
2024-10-10 05:23:05 -0600

    Cambios

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ class TestUrbanRoutes:
         rate_page = UrbanRoutesPage.UrbanRoutesPage(self.driver)
         rate_page.click_taxi_button()
         rate_page.click_comfort_button()
-        #assert rate_page.get_comfort_specs() == 'Manta y pañuelos'
+        assert rate_page.get_comfort_specs() == 'Manta y pañuelos'
 
     def test_phone_interaction_and_filling(self):
         form_page = UrbanRoutesPage.UrbanRoutesPage(self.driver)
         form_page.click_phone_interaction()
         number_phone = data.phone_number
         form_page.filling_phone_field(number_phone)
-        #assert form_page.get_phone() == number_phone
+        assert form_page.get_phone() == number_phone
 
     def test_phone_code_confirmation(self):
         phone_form = UrbanRoutesPage.UrbanRoutesPage(self.driver)
@@ -59,8 +59,8 @@ class TestUrbanRoutes:
         cvv_code = data.card_code
         card_form.filling_card_field(card_data)
         card_form.filling_cvv_field(cvv_code)
-        #assert card_form.get_card_number() == card_number
-        #assert card_form.get_cvv_number() == card_code
+        assert card_form.get_card_number() == card_number
+        assert card_form.get_cvv_number() == card_code
         card_form.click_add_card_confirmation_button()
         card_form.click_close_card_add_interaction(2)
 
@@ -68,18 +68,18 @@ class TestUrbanRoutes:
         comments_form = UrbanRoutesPage.UrbanRoutesPage(self.driver)
         comment = data.message_for_driver
         comments_form.add_comment_to_driver(comment)
-        #assert comments_form.get_comment_to_driver() == comment
+        assert comments_form.get_comment_to_driver() == comment
 
     def test_blanket_request(self):
         requests_form = UrbanRoutesPage.UrbanRoutesPage(self.driver)
         requests_form.click_switch_blanket()
-        #assert requests_form.switch_blanket.is_selected() == True
+        assert requests_form.switch_blanket.is_selected() == True
 
 
     def test_ice_cream_request(self):
         requests_form = UrbanRoutesPage.UrbanRoutesPage(self.driver)
         requests_form.add_ice_cream()
-        #assert requests_form.get_ice_cream_value() == 2
+        assert requests_form.get_ice_cream_value() == 2
 
     def test_wait_for_taxi_modal_to_be_displayed(self):
         service_confirmation = UrbanRoutesPage.UrbanRoutesPage(self.driver)
